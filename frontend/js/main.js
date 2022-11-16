e262c66a16deee104a862dd1f64e324e69f229f9
AKSHAY NAMDEO ADATE <adate@AKSHAYs-MacBook-Pro.local>
2022-11-16 16:59:13 +0530

    push

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,67 +1,15 @@
-
-function validate() {
-    var input = $('.validate-input .input100');
-    var check = true;
-    for (var i = 0; i < input.length; i++) {
-        if (notEmpty(input) == false) {
-            showValidate(input[i]);
-            check = false;
-        }
-    }
-
-
-    $('.validate-form .input100').each(function () {
-        $(this).focus(function () {
-            hideValidate(this);
-        });
-    });
-
-    return check;
-}
-
-
-function notEmpty(input) {
-    if ($(input).val().trim() == '') {
-        return false;
-    }
-    return true;
-}
-
-function showValidate(input) {
-    var thisAlert = $(input).parent();
-
-    $(thisAlert).addClass('alert-validate');
-}
-
-function hideValidate(input) {
-    var thisAlert = $(input).parent();
-
-    $(thisAlert).removeClass('alert-validate');
-}
-
 function login() {
-    if (validate()) {
-        let input = $('.validate-input .input100');
-        let username;
-        let password;
-        for (var i = 0; i < input.length; i++) {
-            if (input[i].name == 'username') {
-                username = input[i].value
-            } else {
-                password = input[i].value
-            }
-
-        }
-        if(checkCredentials(username, password)){
-            window.location ='portal.html'
-        }
-
-        // call function here for login
-        // console.log('login called', username, password);
+    const username = $('.username').val();
+    const password = $('.password').val();
+    if(checkCredentials(username, password)){
+        console.log(true)
+        $.cookie('login', 'admin');
     }
 }
 
 function checkCredentials(user, pass) {
-    // check validation crdentials
-    return true
+    if (user === 'admin' && pass === 'admin') {
+        return true;
+    }
+    return false
 }
