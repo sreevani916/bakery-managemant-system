0f30c0f2cb5d3e471644635f555989dfe393f4f0
AKSHAY NAMDEO ADATE <adate@AKSHAYs-MacBook-Pro.local>
2022-11-23 19:07:25 +0530

    [FIX] Fixed double request issue

--- frontend/js/actions.js
+++ frontend/js/actions.js
@@ -20,31 +20,30 @@ const addOrders = async () => {
         },
         body: JSON.stringify({
             "Custid": "8265",
-            "orderId": "53322",
+            "orderId": "2342r",
             "Ordername": "abcd",
             "Orderprice": "abcd",
             "orderDate": "12-12-2022",
             "deliveryDate": "12-12-2022"
         })
+    }).then(data=> {
+        if(data['status']==201){
+            console.log('duplicate')
+        }else{
+            console.log('order added')
+        }
     }).catch(err => console.log(err));
-    const content = await addOrders.json();
-    if (content.process) {
-        console.log('process done', content.message)
-    } else {
-        console.log('something ', content.message)
-    }
 }
-addOrders();
 
 const addCustomer = async () => {
-    const addCustomer = await fetch('http://localhost:3000/addCustomers', {
+     const addCustomer = await fetch('http://localhost:3000/addCustomers', {
         method: 'POST',
         headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({
-            "Custid": "2212",
+            "Custid": "26tv",
             "custFirstName": "pqer",
             "custSecondName": "fghj",
             "custEmail": "ghjk",
@@ -52,17 +51,14 @@ const addCustomer = async () => {
             "custAddress": "bhjk"
 
         })
+    }).then(data=> {
+        if(data['status']==201){
+            console.log('duplicate')
+        }else{
+            console.log('customer added')
+        }
     }).catch(err => console.log(err));
-    const content = await addCustomer.json();
-    if (content.process) {
-        console.log('process done')
-    } else {
-        console.log('duplicate value')
-    }
 }
-
-
-
 
 const addExpense = async () => {
     const addExpense = await fetch('http://localhost:3000/addExpenses', {
@@ -95,13 +91,13 @@ const deleteOrder = async () => {
         body: JSON.stringify({
             "orderid": "abcd"
         })
+    }).then(data=>{
+        if (data['status']==200) {
+            console.log('Data deleted')
+        } else {
+            console.log('duplicate value')
+        }
     }).catch(err => console.log(err));
-    const content = await deleteOrder.json();
-    if (content.process) {
-        console.log('Data deleted')
-    } else {
-        console.log('duplicate value')
-    }
 }
 
 const deleteCustomer = async () => {
@@ -114,31 +110,11 @@ const deleteCustomer = async () => {
         body: JSON.stringify({
             "custid": "1223"
         })
+    }).then(data=>{
+        if (data['status']) {
+            console.log('Data deleted')
+        } else {
+            console.log('duplicate value')
+        }
     }).catch(err => console.log(err));
-    const content = await deleteCustomer.json();
-    if (content.process) {
-        console.log('Data deleted')
-    } else {
-        console.log('duplicate value')
-    }
 }
-
-// console.log(getOrders());
-
-
-// (async () => {
-    // $(window).on("load", async function () {
-    //     const orderData = await getOrders();
-    //     // const finalOrderData=orderData
-    //     console.log(orderData[0])
-    //     if(orderData[0])
-
-    //     return
-    //     if (orderData[0]) {
-    //         orderData[0].forEach(e => {
-    //             console.log(e)
-    //         });
-    //     }
-        // console.log($('.order-table'))
-    // });
-// })()
